be8b5d70951e1451d5de5aa7dd79d3151c5e124d
Ali Rushdan Tariq <alirtariq@gmail.com>
2015-11-15 20:26:11 -0500

    lecture 15

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('stockApp.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -41,16 +41,20 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+.controller('MyStocksCtrl', ['$scope', 
+  function($scope) {
+    $scope.myStocksArray = [
+      {ticker: "AAPL"},
+      {ticker: "GPRO"},
+      {ticker: "FB"},
+      {ticker: "MSFT"},
+      {ticker: "C"},
+      {ticker: "GE"}
+    ];
+}])
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('StockCtrl', ['$scope', '$stateParams',
+ function($scope, $stateParams) {
+
+  $scope.ticker = $stateParams.stockTicker;
+}]);
